d3bbdad0379a8ca4c54b769648f37cdca58f504b
Az62560 <justincornu@gmail.com>
2025-09-03 11:34:07 +0200

    MAJ photos, textes, ajout footer

---
+++ src/Controller/LegalNoticesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class LegalNoticesController extends AbstractController
+{
+    #[Route('/mentions-légales', name: 'app_legal_notices')]
+    public function index(): Response
+    {
+        return $this->render('legal_notices/index.html.twig');
+    }
+}
